ec01f673cb8d6e0ab2065dd1ae94e7bbdf4586ad
Richard Unger <richard.unger@nexum.com>
2022-09-10 23:14:17 +0200

    add second motor/sensor

--- src/main.cpp
+++ src/main.cpp
@@ -44,13 +44,13 @@ BLDCDriver6PWM driver0 = BLDCDriver6PWM(M0_INUH_PIN, M0_INUL_PIN, M0_INVH_PIN, M
 BLDCMotor motor0 = BLDCMotor(MOTOR_PP, MOTOR_PHASE_RESISTANCE, MOTOR_KV);
 
 // motor 1
-Encoder sensor1 = Encoder(ENCODER1_A_PIN, ENCODER1_B_PIN, 1024, ENCODER1_Z_PIN);
+Encoder sensor1 = Encoder(ENCODER1_A_PIN, ENCODER1_B_PIN, 1024); //, ENCODER1_Z_PIN);
 void handleA1() { sensor1.handleA(); };
 void handleB1() { sensor1.handleB(); };
 void handleZ1() { sensor1.handleIndex(); };
 BLDCDriver6PWM driver1 = BLDCDriver6PWM(M1_INUH_PIN, M1_INUL_PIN, M1_INVH_PIN, M1_INVL_PIN, M1_INWH_PIN, M1_INWL_PIN);
 //LowsideCurrentSense current1 = LowsideCurrentSense(1.0f, 1.0f/CURRENT_VpA, M1_AOUTU_PIN, M1_AOUTV_PIN, M1_AOUTW_PIN);
-BLDCMotor motor1 = BLDCMotor(MOTOR_PP, MOTOR_PHASE_RESISTANCE);
+BLDCMotor motor1 = BLDCMotor(MOTOR_PP, MOTOR_PHASE_RESISTANCE, MOTOR_KV);
 
 // Commander
 Commander commander = Commander(Serial, '\n', false);
@@ -68,7 +68,7 @@ unsigned long ts = 0;
 
 void setup() {
   // setup LEDs
-  leds.init(50);
+  leds.init(100);
   // initialize serial port on USB, debug output goes to this
   Serial.begin(SERIAL_SPEED);
   SimpleFOCDebug::enable();
@@ -86,29 +86,30 @@ void setup() {
   if (!driver0.init())
     SimpleFOCDebug::println("Driver 0 init failed!");
   motor0.linkDriver(&driver0);
-  // SimpleFOCDebug::println("Initializing driver 1...");
-  // if (!driver1.init())
-  //   SimpleFOCDebug::println("Driver 1 init failed!"); 
-  // motor1.linkDriver(&driver1);
+  SimpleFOCDebug::println("Initializing driver 1...");
+  if (!driver1.init())
+    SimpleFOCDebug::println("Driver 1 init failed!"); 
+  motor1.linkDriver(&driver1);
 
   sensor0.init();
   //sensor0.enableInterrupts(handleA0, handleB0, handleZ0);
-  //sensor1.init();
-  //sensor1.enableInterrupts(handleA1, handleB1, handleZ1);
+  sensor1.pullup = Pullup::USE_INTERN;
+  sensor1.init();
+  sensor1.enableInterrupts(handleA1, handleB1, handleZ1);
   leds.signalInitState(1);
 
   motor0.voltage_limit = motor1.voltage_limit = DEFAULT_VOLTAGE_LIMIT / 2.0f;
   motor0.voltage_sensor_align = motor1.voltage_sensor_align = ALIGNMENT_VOLTAGE_LIMIT;
   motor0.velocity_limit = motor1.velocity_limit = 200.0f; // 200rad/s is pretty fast
   motor0.PID_velocity.P = motor1.PID_velocity.P = 0.2f;
-  motor0.PID_velocity.I = motor1.PID_velocity.I = 2.0f;
+  motor0.PID_velocity.I = motor1.PID_velocity.I = 0.6f;
   motor0.PID_velocity.D = motor1.PID_velocity.D = 0.0f;
-  motor0.PID_velocity.output_ramp = motor1.PID_velocity.output_ramp = 1000.0f;
+  motor0.PID_velocity.output_ramp = motor1.PID_velocity.output_ramp = 200.0f;
   //motor0.PID_velocity.limit = motor1.PID_velocity.limit = DEFAULT_VOLTAGE_LIMIT; // TODO check this
   motor0.P_angle.P = motor1.P_angle.P = 20.0f;
-  motor0.LPF_velocity.Tf = motor1.LPF_velocity.Tf = 0.01f;
+  motor0.LPF_velocity.Tf = motor1.LPF_velocity.Tf = 0.05f;
   motor0.foc_modulation = motor1.foc_modulation = FOCModulationType::SinePWM;
-  motor0.controller = motor1.controller = MotionControlType::velocity_openloop;
+  motor0.controller = motor1.controller = MotionControlType::velocity;
   motor0.torque_controller = motor1.torque_controller = TorqueControlType::voltage;
 
   motor0.motion_downsample = motor1.motion_downsample = 4;
@@ -127,8 +128,10 @@ void setup() {
     if (motor0.motor_status==FOCMotorStatus::motor_uncalibrated) {
       motor0.linkSensor(&sensor0);
       SimpleFOCDebug::println("Initializing FOC motor 0...");
-      motor0.initFOC();
-      SimpleFOCDebug::println("Motor 0 intialized.");
+      if (motor0.initFOC())
+        SimpleFOCDebug::println("Motor 0 ready for closed loop.");
+      else
+        SimpleFOCDebug::println("Motor 0 ready for open loop.");        
       leds.signalInitState(3);
     }
     else
@@ -145,10 +148,12 @@ void setup() {
     // else
     //   motor1.linkCurrentSense(&current1);
     if (motor1.motor_status==FOCMotorStatus::motor_uncalibrated) {
-        motor1.linkSensor(&sensor1);
+        //motor1.linkSensor(&sensor1);
         SimpleFOCDebug::println("Initializing FOC motor 1...");
-        motor1.initFOC();
-        SimpleFOCDebug::println("Motor 1 intialized.");
+        if (motor1.initFOC())
+          SimpleFOCDebug::println("Motor 1 ready for closed loop.");
+        else
+          SimpleFOCDebug::println("Motor 1 ready for open loop.");
     }
     else
       SimpleFOCDebug::println("Motor 1 init failed!");
@@ -168,26 +173,34 @@ void setup() {
 
 
 void loop() {
-  if (motor0.motor_status==FOCMotorStatus::motor_uncalibrated || motor0.motor_status==FOCMotorStatus::motor_ready)
+
+  if (motor0.motor_status==FOCMotorStatus::motor_uncalibrated 
+      || motor0.motor_status==FOCMotorStatus::motor_calib_failed 
+      || motor0.motor_status==FOCMotorStatus::motor_ready)
     motor0.move();
-  if (motor1.motor_status==FOCMotorStatus::motor_uncalibrated || motor1.motor_status==FOCMotorStatus::motor_ready)
+
+  if (motor1.motor_status==FOCMotorStatus::motor_uncalibrated 
+      || motor1.motor_status==FOCMotorStatus::motor_calib_failed 
+      || motor1.motor_status==FOCMotorStatus::motor_ready)
     motor1.move();
+
   if (motor0.motor_status==FOCMotorStatus::motor_ready)
     motor0.loopFOC();
   else
     sensor0.update();
+
   if (motor1.motor_status==FOCMotorStatus::motor_ready)
     motor1.loopFOC();
-  // else
-  //   sensor1.update();
+  else
+    sensor1.update();
 
   count++;
   if (ts + 1000uL < millis()) {
     ts = millis();
     SimpleFOCDebug::print("loop/s: ");
     SimpleFOCDebug::print(count);
-    SimpleFOCDebug::print(" ang0: ");
-    SimpleFOCDebug::println(sensor0.getAngle());
+    SimpleFOCDebug::print(" ang1: ");
+    SimpleFOCDebug::println(sensor1.getAngle());
     count = 0;
   }
 
